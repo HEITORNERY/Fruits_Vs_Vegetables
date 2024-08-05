dfd5aa1abaf8a40112ea2d225dc9e420c8d479dd
HEITORNERY <joseheitorneryviana@gmail.com>
2024-08-05 08:52:28 -0300

    Desisto

--- scripts/game_level.gd
+++ scripts/game_level.gd
@@ -1,11 +1,7 @@
 extends Node2D
 func _ready():
 	for children in get_tree().root.get_children():
-<<<<<<< HEAD
 		if children is ItensDropados:
-=======
-		if children is Area2D:
->>>>>>> a4b859ff882126635df3c9e91f3319d20924b721
 			children.queue_free()
 	global.money = 0
 	global.money_gasto = 0
@@ -14,7 +10,6 @@ func _ready():
 	global.enemies_killed = 0
 	global.damage_maked = 0
 	global.damage_suffer = 0
-<<<<<<< HEAD
 	global.unlock_weapons = {
 	"big_sword": 1,
 	"bow": 0,
@@ -23,5 +18,3 @@ func _ready():
 	"spear": 0,
 	"ax":0
 }
-=======
->>>>>>> a4b859ff882126635df3c9e91f3319d20924b721
